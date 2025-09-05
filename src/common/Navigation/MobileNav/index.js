2cfc0ad6113610981e8092cdea02c2da2ba07c93
Donata Deptula <dondeptu@gmail.com>
2025-09-06 01:15:34 +0200

    Added NavBar for mobile view

--- src/common/Navigation/MobileNav/index.js
+++ src/common/Navigation/MobileNav/index.js
@@ -1,7 +1,12 @@
-import { NavBar } from "./styled";
+import { toHomepage } from "../../../core/App/routes";
+import { BurgerMenu, LogoNavLink, NavBar, StyledLogo, Title } from "./styled";
 
 export const MobileNav = () => (
     <NavBar>
-        
+        <LogoNavLink to={toHomepage()}>
+            <StyledLogo alt="Logo" />
+            <Title>Rust Poland</Title>
+        </LogoNavLink>
+        <BurgerMenu />
     </NavBar>
 );
--- src/common/Navigation/MobileNav/styled.js
+++ src/common/Navigation/MobileNav/styled.js
@@ -1,3 +1,37 @@
 import styled from "styled-components";
+import { ReactComponent as Logo } from "../logo.svg";
+import { ReactComponent as BurgerMenuIcon } from "./burger-menu.svg";
+import { NavLink } from "react-router";
 
-export const NavBar = styled.nav``;
+export const NavBar = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+`;
+
+export const LogoNavLink = styled(NavLink)`
+    display: flex;
+    align-items: flex-end;
+    gap: 16px;
+    text-decoration: none;
+`;
+
+export const StyledLogo = styled(Logo)`
+    width: 41px;
+    height: 44px;
+`;
+
+export const Title = styled.span`
+    font-family: ${({ theme }) => theme.fonts.logo};
+    font-weight: 700;
+    font-size: 18px;
+    color: ${({ theme }) => theme.page.text};
+
+    &:active {
+        color: ${({ theme }) => theme.navigation.active};
+    }
+`;
+
+export const BurgerMenu = styled(BurgerMenuIcon)`
+    width: 27px;
+`;
