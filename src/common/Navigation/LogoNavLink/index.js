c06ec178462b0ea6ce70649bd12f6b6c5f76f4ce
Donata Deptula <dondeptu@gmail.com>
2025-09-06 06:53:03 +0200

    added aria-label to LogoNavLink for accessibility

--- src/common/Navigation/LogoNavLink/index.js
+++ src/common/Navigation/LogoNavLink/index.js
@@ -2,7 +2,7 @@ import { toHomepage } from "../../../core/App/routes";
 import { LogoLink, StyledLogo, Title } from "./styled";
 
 export const LogoNavLink = ({ variant }) => (
-    <LogoLink $variant={variant} to={toHomepage()}>
+    <LogoLink $variant={variant} to={toHomepage()} aria-label="Go to homepage">
         <StyledLogo $variant={variant} alt="Logo" />
         <Title $variant={variant}>Rust Poland</Title>
     </LogoLink>
