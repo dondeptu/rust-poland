8316d830340ff5476098f41b28576269137156d1
Donata Deptula <dondeptu@gmail.com>
2025-09-06 04:48:02 +0200

    refactor MobileMenu to full-screen overlay

--- src/common/Navigation/MobileNav/DropdownMenu/index.js
+++ src/common/Navigation/MobileNav/DropdownMenu/index.js
@@ -1,22 +1,28 @@
 import { becomeSpeaker, meetup } from "../../../../core/App/externalLinks";
 import { toArticles } from "../../../../core/App/routes";
-import { Item, MenuList, MenuLink, StyledNavLink } from "./syled";
+import { Item, List, MenuLink, StyledNavLink, MobileMenu, CloseMenuIcon, StyledLogo, MenuList } from "./syled";
 
 export const DropdownMenu = () => {
     return (
-        <MenuList>
-            <Item>
-                <MenuLink href={meetup()} target="_blank" rel="noopener noreferrer">Meetup</MenuLink>
-            </Item>
-            <Item>
-                <StyledNavLink to={toArticles()}>Articles</StyledNavLink>
-            </Item>
-            <Item>
-                <MenuLink href={becomeSpeaker()} target="_blank" rel="noopener noreferrer">Become a Speaker!</MenuLink>
-            </Item>
-            <Item>
-                <MenuLink href="#footer">Contact Us</MenuLink>
-            </Item>
-        </MenuList>
+        <MobileMenu aria-label="Mobile menu">
+            <MenuList>
+                <CloseMenuIcon />
+                <List>
+                    <Item>
+                        <MenuLink href={meetup()} target="_blank" rel="noopener noreferrer">Meetup</MenuLink>
+                    </Item>
+                    <Item>
+                        <StyledNavLink to={toArticles()}>Articles</StyledNavLink>
+                    </Item>
+                    <Item>
+                        <MenuLink href={becomeSpeaker()} target="_blank" rel="noopener noreferrer">Become a Speaker!</MenuLink>
+                    </Item>
+                    <Item>
+                        <MenuLink href="#footer">Contact Us</MenuLink>
+                    </Item>
+                </List>
+            </MenuList>
+            <StyledLogo />
+        </MobileMenu>
     );
 }
--- src/common/Navigation/MobileNav/DropdownMenu/syled.js
+++ src/common/Navigation/MobileNav/DropdownMenu/syled.js
@@ -1,14 +1,37 @@
 import { NavLink } from "react-router";
 import styled, { css } from "styled-components";
+import { ReactComponent as CloseIcon } from "./close-menu.svg";
+import { ReactComponent as Logo } from "../../logo.svg";
 
-export const MenuList = styled.ul`
+export const MobileMenu = styled.nav`
     position: fixed;
     z-index: 10;
-    top: 93px;
+    top: 0;
     left: 0;
     width: 100%;
+    height: 100vh;
+    background-color: ${({ theme }) => theme.page.background};
+    display: flex;
+    flex-direction: column;
+    padding-top: 52px;
+    padding-bottom: 46px;
+    justify-content: space-between;
+`;
+
+export const CloseMenuIcon = styled(CloseIcon)`
+    width: 27px;
+    align-self: flex-end;
+    margin-right: 9px;
+`;
+
+export const MenuList = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const List = styled.ul`
     padding: 0;
-    margin: 0;
+    margin: 10px 0;
     font-family: ${({ theme }) => theme.fonts.headers};
     font-size: 18px;
 `;
@@ -38,4 +61,10 @@ export const StyledNavLink = styled(NavLink)`
 
 export const MenuLink = styled.a`
     ${navBarLinksStyles}
+`;
+
+export const StyledLogo = styled(Logo)`
+    width: 41px;
+    height: 44px;
+    align-self: center;
 `;
