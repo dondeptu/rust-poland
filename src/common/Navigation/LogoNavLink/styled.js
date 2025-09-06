50dd2673dd2bc6e867b4233b19de96aa53c32587
Donata Deptula <dondeptu@gmail.com>
2025-09-06 02:57:10 +0200

    add $variant prop for LogoNavLink

--- src/common/Navigation/LogoNavLink/styled.js
+++ src/common/Navigation/LogoNavLink/styled.js
@@ -5,21 +5,21 @@ import { NavLink } from "react-router";
 export const LogoLink = styled(NavLink)`
     display: flex;
     align-items: flex-end;
-    gap: ${({ variant }) => variant === "mobile" ? "16px" : "14px"};
+    gap: ${({ $variant }) => $variant === "mobile" ? "16px" : "14px"};
     text-decoration: none;
 `;
 
 export const StyledLogo = styled(Logo)`
-    width: ${({ variant }) => variant === "mobile" ? "41px" : "76px"};
-    height: ${({ variant }) => variant === "mobile" ? "44px" : "auto"};
+    width: ${({ $variant }) => $variant === "mobile" ? "41px" : "76px"};
+    height: ${({ $variant }) => $variant === "mobile" ? "44px" : "auto"};
 `;
 
 export const Title = styled.span`
     font-family: ${({ theme }) => theme.fonts.logo};
     font-weight: 700;
-    font-size: ${({ variant }) => variant === "mobile" ? "18px" : "20px"};
+    font-size: ${({ $variant }) => $variant === "mobile" ? "18px" : "20px"};
     color: ${({ theme }) => theme.page.text};
-    padding-bottom: ${({ variant }) => variant === "mobile" ? "0" : "12px"};
+    padding-bottom: ${({ $variant }) => $variant === "mobile" ? "0" : "12px"};
 
     &:active {
         color: ${({ theme }) => theme.navigation.active};
