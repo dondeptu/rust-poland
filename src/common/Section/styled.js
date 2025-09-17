d2777d394707c5c8e68429759020f4bae6c905d4
Donata Deptula <dondeptu@gmail.com>
2025-09-17 19:51:32 +0200

    Added media queries to Section

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -3,9 +3,19 @@ import styled from "styled-components";
 export const Header = styled.h2`
     font-family: ${({ theme }) => theme.fonts.headers};
     font-size: 20px;
-    font-weight: 600;
+    font-weight: 700;
     letter-spacing: 0.1em;
     text-transform: uppercase;
     margin-top: 0;
     margin-bottom: 16px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.tabletMax}px) {
+        font-size: 18px;
+        margin-bottom: 12px;
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        font-size: 16px;
+        margin-bottom: 8px;
+    }
 `;
