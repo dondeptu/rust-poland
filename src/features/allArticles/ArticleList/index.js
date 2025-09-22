3c063d52b7087842c360cf0d1fc995cb8fa44d4c
Donata Deptula <dondeptu@gmail.com>
2025-09-22 23:04:44 +0200

    Articles list layout

--- src/features/allArticles/ArticleList/index.js
+++ src/features/allArticles/ArticleList/index.js
@@ -1,23 +1,42 @@
-import { Section } from "../../../common/Section";
 import { ArticleBox } from "./ArticleBox";
-import { YearHeader, StyledLine } from "./styled";
+import { YearHeader, StyledLine, List, ArticleSection, Year, ArticlesOfYear } from "./styled";
 
 export const ArticleList = () => (
-    <Section
-        header={
+    <ArticleSection>
+        <ArticlesOfYear>
             <YearHeader>
-                2025
+                <Year>{"2025"}</Year>
                 <StyledLine />
             </YearHeader>
-        }
-        content={
-            <ArticleBox
-                releaseDate={"13 January"}
-                title={"Rust Poland is online!"}
-                abstact={
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin luctus neque elit, et mattis dui auctor a. Integer eget sem finibus, ullamcorper leo at, ullamcorper felis. Donec condimentum elit libero, consequat pellentesque nibh porta in."
-                }
-            />
-        }
-    />
+            <List>
+                {Array.from({ length: 4 }).map((_, i) => (
+                    <ArticleBox key={i}
+                        releaseDate={"13 January"}
+                        title={"Rust Poland is online!"}
+                        abstact={
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin luctus neque elit, et mattis dui auctor a. Integer eget sem finibus, ullamcorper leo at, ullamcorper felis. Donec condimentum elit libero, consequat pellentesque nibh porta in."
+                        }
+                    />
+                ))}
+            </List>
+        </ArticlesOfYear>
+
+        <ArticlesOfYear>
+            <YearHeader>
+                <Year>{"2024"}</Year>
+                <StyledLine />
+            </YearHeader>
+            <List>
+                {Array.from({ length: 5 }).map((_, i) => (
+                    <ArticleBox key={i}
+                        releaseDate={"13 January"}
+                        title={"Rust Poland is online!"}
+                        abstact={
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin luctus neque elit, et mattis dui auctor a. Integer eget sem finibus, ullamcorper leo at, ullamcorper felis. Donec condimentum elit libero, consequat pellentesque nibh porta in."
+                        }
+                    />
+                ))}
+            </List>
+        </ArticlesOfYear>
+    </ArticleSection>
 );
--- src/features/allArticles/ArticleList/styled.js
+++ src/features/allArticles/ArticleList/styled.js
@@ -1,10 +1,27 @@
 import styled from "styled-components";
 
+export const ArticleSection = styled.section`
+    margin-top: 136px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.tabletMax}px) {
+        margin-top: 90px;
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        margin-top: 56px;
+    }
+`;
+
+export const ArticlesOfYear = styled.div`
+    margin: 16px 0;
+`;
+
 export const YearHeader = styled.div`
     display: grid;
     grid-template-columns: auto 1fr;
     align-items: center;
     gap: 20px;
+    margin-bottom: 16px;
 
     @media(max-width: ${({ theme }) => theme.breakpoints.tabletMax}px) {
         gap: 15px;
@@ -15,6 +32,30 @@ export const YearHeader = styled.div`
     }
 `;
 
+export const Year = styled.h2`
+    font-family: ${({ theme }) => theme.fonts.headers};
+    font-size: 20px;
+    font-weight: 700;
+    letter-spacing: 0.1em;
+    text-transform: uppercase;
+    margin: 0;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.tabletMax}px) {
+        font-size: 18px;
+        margin-bottom: 12px;
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        font-size: 16px;
+        margin-bottom: 10px;
+    }
+`;
+
 export const StyledLine = styled.div`
     border-bottom: 1px solid ${({ theme }) => theme.page.text};
+`;
+
+export const List = styled.div`
+    display: grid;
+    gap: 18px;
 `;
