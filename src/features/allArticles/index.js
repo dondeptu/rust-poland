fce5ea08d109997b654f5f4bb5f0210c6901dd20
Donata Deptula <dondeptu@gmail.com>
2025-09-25 02:07:55 +0200

    Added ContactUs footer component to AllArticles subpage

--- src/features/allArticles/index.js
+++ src/features/allArticles/index.js
@@ -1,9 +1,11 @@
 import { ArticleList } from "./ArticleList";
 import { Header } from "./Header";
+import { ContactUs } from "../../common/ContactUs";
 
 export const AllArticles = () => (
     <main>
         <Header />
         <ArticleList />
+        <ContactUs />
     </main>
 );
