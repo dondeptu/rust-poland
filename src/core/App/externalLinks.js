2598917199ff68f3477c5fa7c72cf3fcf1cb8fd0
Donata Deptula <dondeptu@gmail.com>
2025-09-26 02:34:11 +0200

    updated linkedin link

--- src/core/App/externalLinks.js
+++ src/core/App/externalLinks.js
@@ -2,6 +2,7 @@ export const meetup = () => "https://www.meetup.com/pl-PL/rust-gdansk/";
 export const becomeSpeaker = () => "https://forms.gle/sTesYgNjifiGh9P38";
 export const applicationForm = () => "https://forms.gle/sTesYgNjifiGh9P38";
 export const facebook = () => "https://www.facebook.com/";
-export const linkedIn = () => "https://www.linkedin.com/";
-// TO DO:
-// discord???
+export const linkedIn = () => "https://www.linkedin.com/company/rust-gdansk/";
+// TO DO: Add correct link to Rust Polska facebook account
+// Does an official page exist?
+// Optional: Consider adding Discord link as well
